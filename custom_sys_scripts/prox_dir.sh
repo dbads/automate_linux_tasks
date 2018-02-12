1ac60fa36363530782d150d67cf51144eb8a2cec
Deepak Bharti <deepakbharti823@gmail.com>
2018-02-12 15:07:34 +0530

    initial commit

---
+++ custom_sys_scripts/prox_dir.sh
@@ -0,0 +1,15 @@
+#get present working directory
+cur_dir=`pwd`
+# echo $cur_dir
+#change dir to where main.sh is
+cd /media/deepak/Entreprise/software/proxyman/ProxyMan-master/
+/media/deepak/Entreprise/software/proxyman/ProxyMan-master/main.sh
+cd $cur_dir
+
+#change dir back to where were after configuring proxy
+# echo $curdir
+
+# if [ $result == 0 ] then
+# 	cd $cur_dir
+# fi
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+Some scripts to automate some very oftenly used tasks on Linux
+
+1. Setting up aliases to navigate through various directories
+2. Change dir to some other dir, perform some job and come back to previous directory
+3. Setting up Ubuntu just in a single command after installation eg. Proxy setup, update, upgrade etc.
+4. many more
