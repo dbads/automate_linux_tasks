b8599a88cc4cf9bd7872cb740c0a681bb180b01f
Deepak Bharti <deepakbharti823@gmail.com>
2018-02-17 11:10:18 +0530

    script to setup a fresh linux system

---
+++ fresh_linux_setup/set_linux_up.sh
@@ -0,0 +1,30 @@
+
+
+
+
+apt-get update
+apt-get upgrade 
+apt-get install ubuntu-restricted-extras 
+
+#Use proxyman for proxy setup, its a great tool developed by a IIITA guy
+
+#install a better media player
+apt-get install vlc
+
+
+#install chrome (This will download the chrome debian package in the dir you run the script from and then installs it)
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+
+
+# in case some error occurs
+apt-get install -f 
+
+
+##Adding new things
+# You got something add here
+
+
+
+
