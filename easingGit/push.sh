979fa42be8df8e552ac95f12145429010c0629da
Deepak Bharti <deepakbharti823@gmail.com>
2018-03-08 00:44:48 +0530

    awesome

--- easingGit/push.sh
+++ easingGit/push.sh
@@ -12,3 +12,26 @@ git add $addthis
 git commit -m $message
 git push origin $branch
 
+
+if [ (!$username)]; then
+	echo "Your Github Username and Password are not set. You can set it now :"
+	read " Username : " username
+	read " password : " password
+fi
+
+username=${GIT_USERNAME}
+password=${GIT_PASSWORD}
+
+
+if [ $username && $password]; then
+	expect ":Username:"
+	send "$username\r";
+	expect ":Password:"
+	send "$password\r";
+	interact
+fi
+
+
+
+
+# echo ${EMAIL_HOST_USER}
