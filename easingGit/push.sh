0e6ab786799fc673eed770dc3cdf6b8bdd043a8f
Deepak Bharti <deepakbharti823@gmail.com>
2018-03-07 23:36:20 +0530

    cool

--- easingGit/push.sh
+++ easingGit/push.sh
@@ -1,1 +1,14 @@
-push.sh
+
+
+read -p " What to add(-A for adding all or a dir name to add) " addthis  
+
+# if [[ (!"$choice" = "-A") || (!"$choice" = "Invalid dir") ) ]]; then
+#     echo "Invalid choice! Start again."
+# fi
+read -p " Commit message : " message
+read -p " Branch to commit : " branch
+
+git add $addthis
+git commit -m $message
+git push origin $branch
+
