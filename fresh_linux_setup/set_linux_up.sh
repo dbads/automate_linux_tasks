fea62581f30113bf25a7afa33c8a3c9b65e9e721
Deepak Bharti <deepakbharti823@gmail.com>
2018-05-13 12:46:39 +0530

    added coding requirements

--- fresh_linux_setup/set_linux_up.sh
+++ fresh_linux_setup/set_linux_up.sh
@@ -8,15 +8,22 @@ apt-get install ubuntu-restricted-extras
 
 #Use proxyman for proxy setup, its a great tool developed by a IIITA guy
 
+
+#Coding requirements
+apt-get install gcc g++ vim
+
+#for java install java development kit (jdk) version 8 or 9 from the below link
+# http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz
+# Installthe package using following command
+tar zxvf package_name
+
 #install a better media player
 apt-get install vlc
-
 
 #install chrome (This will download the chrome debian package in the dir you run the script from and then installs it)
 sudo apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
-
 
 # in case some error occurs
 apt-get install -f 
